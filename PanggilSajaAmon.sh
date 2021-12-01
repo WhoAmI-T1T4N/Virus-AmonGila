81b25a0c8758ce33800cad31882aec9bf316a667
WhoAmI-T1T4N <92716161+WhoAmI-T1T4N@users.noreply.github.com>
2021-12-01 16:16:31 +0700

    Create PanggilSajaAmon.sh

---
+++ PanggilSajaAmon.sh
@@ -0,0 +1,211 @@
+# Virus Kang Tebas
+# Si Amon Jadi Populer
+#[---Author : T1T4N TZY---]
+
+gn='\033[0;32m'
+gl='\033[32;1m'
+rd='\033[31;1m'
+wi='\033[37;1m'
+bk='\033[30;1m'
+yw='\033[33;1m'
+yl='\033[1;33m'
+be='\033[36;1m'
+bl='\033[0;36m'
+
+# user & pass
+clear
+echo -e $bl "Masukan Username Anda"
+     read -p "User =>" user
+sleep 5
+echo -e $rd "USER Telah Di Verifikasi"
+echo -e $be "Masukan Password Anda"
+     read -p "Pass =>" pass
+sleep 5
+echo -e $rd "PASS Telah Di Verifikasi"
+# tampilan
+clear
+echo -e $bk "########################"
+echo -e $gl "# SELAMAT DATANG" $user $gl"#"
+echo -e $bk "########################"
+echo -e $yl "Jangan Lupa Subscribe T1T4N TZY"
+echo
+echo -e $bk "#########################"
+echo -e $rd "1]. Virus Amon"
+echo -e $rd "2]. Virus Kelelawar"
+echo -e $rd "3]. Virus Si Kratos"
+echo -e $rd "4]. Virus Pasukan Farel"
+echo -e $rd "5]. Keluar Aja"
+echo -e $bk "#########################"
+echo -e $gn "Pilih Virus Dari Kang Tebas"
+     read -p "Virus =>" vp
+if [[ $vp == 1 || $vp == 1 ]]
+then
+clear
+  echo -e $gn "Masukan User Anda Lagi"
+  echo -e $gn "Masukan Pass Anda Lagi"
+  read -p "User =>" kl
+  sleep 5
+  echo -e $gn "User Terverifikasi"
+  read -p "Pass =>" kp
+  sleep 5
+  echo -e $gn "Pass Terverifikasi"
+ clear
+ echo -e $bk "@@@@@@@@@@@@@@@@@@@@"
+ echo -e $yl "1]. Kirim Sekarang"
+ echo -e $bk "@@@@@@@@@@@@@@@@@@@@"
+     read -p "Kirim ? =>" anjay
+fi
+if [[ $anjay || $anjay ]]
+then
+clear
+    read -p "KirimKan Kemana? =>" km
+clear
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Amon Sedang Di Kirim Ke" $km
+fi
+if [[ $vp == 2 || $vp == 2 ]]
+then
+clear
+echo -e $gn "Masukan User Anda Lagi"
+  echo -e $gn "Masukan Pass Anda Lagi"
+  read -p "User =>" kl
+  sleep 5
+  echo -e $gn "User Terverifikasi"
+  read -p "Pass =>" kp
+  sleep 5
+  echo -e $gn "Pass Terverifikasi"
+ clear
+ echo -e $bk "@@@@@@@@@@@@@@@@@@@@"
+ echo -e $yl "1]. Kirim Sekarang"
+ echo -e $bk "@@@@@@@@@@@@@@@@@@@@"
+     read -p "Kirim ? =>" pp
+fi
+if [[ $pp == 2 || $pp == 2 ]]
+then
+clear
+ read -p "Kirimkan Kemana? =>" pop
+clear
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+sleep 5
+echo -e $bl "Virus Kelelawar Sedang Di Kirim Ke" $pop
+fi
+if [[ $vp == 3 || $vp == 3 ]]
+then
+clear
+echo -e $gn "Masukan User Anda Lagi"
+  echo -e $gn "Masukan Pass Anda Lagi"
+  read -p "User =>" kl
+  sleep 5
+  echo -e $gn "User Terverifikasi"
+  read -p "Pass =>" kp
+  sleep 5
+  echo -e $gn "Pass Terverifikasi"
+ clear
+ echo -e $bk "@@@@@@@@@@@@@@@@@@@@"
+ echo -e $yl "1]. Kirim Sekarang"
+ echo -e $bk "@@@@@@@@@@@@@@@@@@@@"
+     read -p "Kirim ? =>" kj
+fi
+if [[ $kj == 1 || $kj == 1 ]]
+then
+clear
+ read -p "Kirimkan Kemana? =>" keju
+clear
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $km
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+sleep 5
+echo -e $bl "Virus Kratos Sedang Di Kirim Ke" $keju
+fi
+
